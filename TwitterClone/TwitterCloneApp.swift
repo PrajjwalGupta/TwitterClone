154cb7cae342f3d48b5f173f32fa8530a28496eb
Prajjwal Gupta <31537066+PrajjwalGupta@users.noreply.github.com>
2022-06-20 15:05:37 +0530

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Prajjawal Gupta on 20/06/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
